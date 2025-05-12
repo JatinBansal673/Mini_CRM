6298d3499cd9b9242e27532fa2e9ea55153dc763
JatinBansal673 <jatinbansal673@gmail.com>
2025-05-12 07:21:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(session({
     maxAge: 24 * 60 * 60 * 1000, // 1 day
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
-    sameSite:"lax", // true only if using HTTPS
+    sameSite:"None", // true only if using HTTPS
   }
 }));
 
